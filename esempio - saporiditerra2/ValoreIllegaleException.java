1e918d1df00d48d085b1d7e29695eeaf0a578bdc
comarandr <138122@spes.uniud.it>
2025-03-23 14:32:59 +0100

    ciao

--- esempio - saporiditerra2/ValoreIllegaleException.java
+++
@@ -1,7 +0,0 @@
-package it.uniud.poo.compiti.saporiditerra2;
-
-public class ValoreIllegaleException extends Exception {
-    public ValoreIllegaleException(String message) {
-        super(message);
-    }
-}
